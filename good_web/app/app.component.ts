e32982d853f4667ef69c5ee6d7f8e1f39cf49d72
Kris Horton <khort@Kriss-MacBook-Pro.local>
2017-02-28 11:59:19 -0500

    Made Login look presentable

--- good_web/app/app.component.ts
+++ good_web/app/app.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'my-app',
     template: `
-    <h1>Good Job</h1>
-  
+    <div align="center"><h1>GoodJob</h1></div>
+
     <router-outlet></router-outlet>
     `
 })
--- good_web/app/login.component.ts
+++ good_web/app/login.component.ts
@@ -12,26 +12,33 @@ import { ActivatedRoute, Router} from '@angular/router';
 @Component({
   selector: 'login-page',
   template: `
+  <div align=center>{{user.name}}</div>
+  <br>
   <form [formGroup]="loginForm" (ngSubmit)="doLogin($event)">
-    <div><input formControlName="email" type="email" placeholder="Your email"></div>
-    <div><input formControlName="password" type="password" placeholder="Your password"></div>
-  <button type="submit">Log in</button>
+    <div align = center><input formControlName="email" type="email" placeholder="Enter Email"></div>
+    <div align = center><input formControlName="password" type="password" placeholder="Enter Password"></div>
+    <br>
+  <div align=center><button type="submit">Log In</button></div>
+  <div style="line-height:50%;">
+    <br>
+</div>
+  <div align=center><a href="create user" [routerLink]="['/create']"><button class="createbutton">Create User</button></a></div>
 </form>
-<div>{{user.name}}</div>
 
-<a href="create user" [routerLink]="['/create']">create user</a>
+
+
   `
 })
 
 export class LoginPage {
   user: HrPerson = this.goodJobService.fetch_null_hr_person()
- 
+
   public loginForm = this.fb.group({
     email: ["", Validators.required],
     password: ["", Validators.required]
   });
 
-  
+
   constructor(public fb: FormBuilder, private goodJobService: GoodJobService, private router: Router) {}
 
   doLogin(event: any) {
@@ -39,10 +46,10 @@ export class LoginPage {
     //this.user = this.goodJobService.get_user()
     //console.log(this.user)
     //if(this.user.name !== "error!"){this.router.navigate(['/app'])}
-   
+
    this.goodJobService.test_login(this.loginForm.value.password, this.loginForm.value.email)
    .subscribe(p => this.router.navigate(['/app']), error=> this.user.name = "login error!")
     console.log(this.user)
   }
-    
+
 }
