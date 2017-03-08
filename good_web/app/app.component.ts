8f69595f6bcec261db825a6c2c1e13f4e2c74e3a
Kris Horton <khort@Kriss-MacBook-Pro.local>
2017-03-08 13:14:53 -0500

    updated_sign_in

--- good_web/app/app.component.ts
+++ good_web/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'my-app',
     template: `
-    <div align="center"><h1>GoodJob</h1></div>
-
+    <div class = "main" align="center"><h1>GoodJob</h1></div>
+    <div class="navbar">
+      <a href="#home">Home</a>
+      <a href="#news">News</a>
+      <a href="#contact">Contact</a>
+      <a href="#about">About</a>
+    </div>
     <router-outlet></router-outlet>
     `
 })
