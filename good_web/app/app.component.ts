5c06606f209bc2e0766a1ade522cbfd9aa4b5ea5
Kris Horton <khort@Kriss-MacBook-Pro.local>
2017-03-21 10:36:57 -0400

    Updates for 3/21

--- good_web/app/app.component.ts
+++ good_web/app/app.component.ts
@@ -3,13 +3,22 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'my-app',
     template: `
-    <div class = "main" align="center"><h1>GoodJob</h1></div>
-    <div class="navbar">
+    <br>
+    <br>
+    <br>
+    <br>
+    <div class="logo" align=center>
+    <img src="GoodJob LogogoFull.png" height="250" width="550">
+    </div>
+  <div class="navbar">
       <a href="#home">Home</a>
       <a href="#news">News</a>
       <a href="#contact">Contact</a>
       <a href="#about">About</a>
+        <a href="#"><button class="logout">Logout</button></a>
+      <div class="logonav"><img src="GoodJob LogogoJustG.png" height="50" width="55"></div>
     </div>
+
     <router-outlet></router-outlet>
     `
 })
--- good_web/app/create_job.component.ts
+++ good_web/app/create_job.component.ts
@@ -14,21 +14,21 @@ import { ActivatedRoute, Router} from '@angular/router';
 
 export class CreateJob {
   job: Job = this.goodJobService.fetch_null_job();
- 
+
   public loginForm = this.fb.group({
     name: ["", Validators.required],
     description: ["", Validators.required],
     salary_range: ["", Validators.required],
     employment_type: ["", Validators.required],
     location: ["", Validators.required],
-    tags: ["", Validators.required]    
+    tags: ["", Validators.required]
   });
 
-  
+
   constructor(public fb: FormBuilder, private goodJobService: GoodJobService, private router: Router) {
     this.job.name = "create a new job!"
   }
-  
+
   newJob(event: any) {
     this.job.name = this.loginForm.value.name
     this.job.company = GoodJobService.hr_person.company
@@ -40,16 +40,16 @@ export class CreateJob {
 
     this.goodJobService.new_job(this.create(this.job))
    .subscribe(p => this.job = p, error=> this.job.name = "error creating job!")
-    
+
     this.job
   }
 
   create(job: Job) :any{
-       let comp = 
+       let comp =
          {
           "new": {
             "company": job.company,
-            "name": job.name, 
+            "name": job.name,
             "description": job.description,
             "salary_range": job.salary_range,
             "employment_type": job.employment_type,
@@ -61,4 +61,3 @@ export class CreateJob {
         return comp
   }
 }
-
